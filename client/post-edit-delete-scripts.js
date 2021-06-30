df98bdbcde2ff704ed77b6db7321b7941daeed65
Brad Snyder <bsnyder13@gmail.com>
2021-06-29 22:57:04 -0400

    Second commit on Journal project

--- client/post-edit-delete-scripts.js
+++ client/post-edit-delete-scripts.js
@@ -23,28 +23,85 @@ function postJournal() {
         }),
         body: JSON.stringify(newEntry)
     })
+        .then(response => response.json())
+        .then(data => {
+            console.log(data)
+            displayMine()
+        })
+        .catch(err => {
+            console.error(err)
+        })
+}
+
+
+/* *************************
+ *** UPDATE JOURNAL ***
+************************** */
+function editJournal(postId) {
+    console.log(postId);
+
+    const fetch_url = `http://localhost:3000/journal/update/${postId}`;
+    const accessToken = localStorage.getItem('sessionToken');
+
+    let card = document.getElementById(postId);
+    let input = document.createElement('input');
+
+    if (card.childNodes.length < 2) {
+        card.appendChild(input);
+        input.setAttribute("type", "text");
+        input.setAttribute("id", "updatedEntry");
+        input.setAttribute("placeholder", "Edit your journal entry");
+    } else {
+
+        let updated = document.getElementById('updatedEntry').value
+        let newEntry = {
+            journal: {
+                entry: updated
+            }
+        }
+
+
+        fetch(fetch_url, {
+            method: "PUT",
+            headers: new Headers({
+                "Content-Type": "application/json",
+                "Authorization": `Bearer ${accessToken}`
+            }),
+            body: JSON.stringify(newEntry)
+        })
+            .then(response => response.json())
+            .then(data => {
+                console.log(data);
+                displayMine();
+            })
+            .catch(err => {
+                console.error(err)
+            })
+        card.removeChild(card.lastChild)
+    }
+}
+/* *************************
+ *** DELETE JOURNAL ***
+************************** */
+function deleteJournal(postId) {
+    console.log(postId);
+
+    const fetch_url = `http://localhost:3000/journal/delete/${postId}`;
+    const accessToken = localStorage.getItem('SessionToken');
+
+    fetch(fetch_url, {
+        method: "DELETE",
+        headers: new Headers({
+            "Content-Type": "application/json",
+            "Authorization": `Bearer ${accessToken}`
+        })
+    })
     .then(response => response.json())
     .then(data => {
-        console.log(data)
-        displayMine()
+        console.log(data);
+        displayMine();
     })
     .catch(err => {
         console.error(err)
     })
 }
-    
-    
-    /* *************************
-     *** UPDATE JOURNAL ***
-    ************************** */
-    function editJournal(postId) {
-     console.log('editJournal Function Called')
-    }
-    
-    
-    /* *************************
-     *** DELETE JOURNAL ***
-    ************************** */
-    function deleteJournal(postId) {
-     console.log('deleteJournal Function Called')
-    }
